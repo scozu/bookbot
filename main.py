2e39fd4695d27c777b007a1f3e601e9fc92f6801
scozu <admin@jasonscholtz.net>
2025-03-23 16:16:13 -0400

    start bookbot project by getting frankenstein text

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print("greetings boots")
+# take filepath to book as input and return the contents as a string
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
 
+# use `get_book_text` to print the contents of "frankenstein"
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    print(book_text)
+
+main()
+
+# accept the contents of the book as a string and return the number of words
