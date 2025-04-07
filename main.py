34e27cd1f29fdd40a85f0b5334b9d927ac5d38c6
scozu <admin@jasonscholtz.net>
2025-04-07 10:46:19 -0400

    Add sort_dict to return sorted list of character count dictionaries, start with print_report, clean up comments

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-from stats import get_num_words, count_characters
+from stats import get_num_words, count_characters, sort_dict
 
 # take filepath to book as input, read it, and return the contents as a string
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         book_text = f.read()
         return book_text
+    
+def print_report(path, word_count, sorted_characters):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path}")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
 
-# Main function that reads the book text from a file, analyzes it using stats functions,
-# and outputs the results to the console
+    # for
+
+# Main function that reads the book text from a file, analyzes it 
+# using stats functions, and outputs the results to the console
 def main():
-    # get the contents of the book
     book_text = get_book_text("books/frankenstein.txt")
     
-    # count the number of words using `get_num_words` from `stats.py`
+    path = "books/frankenstein.txt"
     word_count = get_num_words(book_text)
-    character_count = count_characters(book_text)
+    # character_count = count_characters(book_text)
+    sorted_characters = sort_dict(book_text)
     
-    # print result
-    print(f"{word_count} words found in the document")
-    print(f"{character_count}")
+    print_report(path, word_count, sorted_characters)
+
 main()
--- stats.py
+++ stats.py
@@ -4,10 +4,13 @@ def get_num_words(book_text):
     words = book_text.split()
     # count words in list
     num_words = len(words)
+    
+    # print(num_words)
     return num_words
 
-# accepts string book text and returns character counts
+# accept string book text and return character counts
 def count_characters(book_text):
+    # empty dictionary for characterc count
     char_count = {}
     
     for characters in book_text:
@@ -15,5 +18,28 @@ def count_characters(book_text):
         if characters in char_count:
             char_count[characters] += 1
         else: char_count[characters] = 1
-        # print(char_count)
+    
+    # print(char_count)
     return char_count
+
+def sort_on(dict):
+    return dict["count"]
+
+def sort_dict(book_text):
+    char_count = count_characters(book_text)
+    char_sort = []
+    
+    # Take the dictionary of characters and their counts and 
+    # return a sorted list of dictionaries.
+    # Each dictionary should have two key-value pairs: 
+    # one for the character itself and one for that character's count.
+    
+    for char, count in char_count.items():
+        char_dict = {"char": char, "count": count}
+        char_sort.append(char_dict)
+    
+    char_sort.sort(reverse=True, key=sort_on)
+
+    # print(char_sort)
+    return char_sort
+    
