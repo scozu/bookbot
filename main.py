f38306457b25b1c804733fc2e4dcb56eeb8cf72c
scozu <admin@jasonscholtz.net>
2025-04-07 15:54:53 -0400

    use sys.argv to accept input arguments

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import sys
 from stats import get_num_words, count_characters, sort_dict
 
 # take filepath to book as input, read it, and return the contents as a string
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         book_text = f.read()
-        return book_text
+        
+    return book_text
     
 def print_report(path, word_count, sorted_characters):
     print("============ BOOKBOT ============")
@@ -13,10 +15,11 @@ def print_report(path, word_count, sorted_characters):
     print(f"Found {word_count} total words")
     print("--------- Character Count -------")
 
-    # loop through sorted dictionary to print counts
+    # loop through sorted dictionary to print characters and their counts
     for char_dicts in sorted_characters:
         char = char_dicts["char"]
         count = char_dicts["count"]
+        # skip over non-alphabetical characters
         if char.isalpha():
             print(f"{char}: {count}")
     
@@ -25,9 +28,15 @@ def print_report(path, word_count, sorted_characters):
 # Main function that reads the book text from a file, analyzes it 
 # using stats functions, and outputs the results to the console
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    # path = "books/frankenstein.txt"
+    # use sys.argv to accept book text paths
     
-    path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    path = sys.argv[1]
+    book_text = get_book_text(path)
     word_count = get_num_words(book_text)
     # character_count = count_characters(book_text)
     sorted_characters = sort_dict(book_text)
